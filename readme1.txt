e4b46773623503a61872978ab179575cb7694e6b
grugabyte <98688177+grugabyte@users.noreply.github.com>
2022-03-11 21:00:50 +1100

    files

--- readme1.txt
+++ readme1.txt
@@ -4,3 +4,4 @@ Written in C and Java by Grug
 Update Log:
 08/03/2022 - creation of project
 09/03/2022 - added additional features
+11/3/22 - added 2 features
--- test3.java
+++ test3.java
@@ -27,6 +27,22 @@ public class test3 {
 			System.out.println("\nThe area of the rectangle is: "+res1);
 
 		}
+		
+		else if(fnc == 3) {
+			System.out.println("Enter side");
+			double sC = scanner.nextDouble();
+			double res = sC * sC;
+			System.out.println("\nThe area of the square is: "+res);
+		}
+
+		else if(fnc == 4) {
+			System.out.println("Enter base ");
+			double base = scanner.nextDouble();
+			System.out.println("Enter height ");
+			double height = scanner.nextDouble();
+			double ans = base * height * 1 / 2;
+			System.out.println("\nThe area of the triangle is: "+ans);
+		}
 		else {
 			System.out.println("Invalid output");
 		}
